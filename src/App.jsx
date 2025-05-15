79cc05da40df4711253e2f51b5e94b1b558ef3b9
Ishwor <ishwortimalsina39@gmail.com>
2025-05-15 20:59:54 +0545

    new config files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+      <div className="rounded-2xl bg-white p-10 text-center shadow-xl">
+        <h1 className="mb-4 text-3xl font-bold text-gray-800">
+          🎉 Tailwind CSS is Working!
+        </h1>
+        <p className="text-gray-600">
+          If you can see this styled box, you&apos;re all set. Now go build
+          something awesome!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+//eslint-disable-next-line
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+
+    extend: {
+      fontSize: {
+        huge: ['10rem', { lineHeight: '1' }],
+      },
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
-}
-
+};
