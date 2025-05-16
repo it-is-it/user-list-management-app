f030d7199737eae832671670d371bf3fa531b02c
Ishwor <ishwortimalsina39@gmail.com>
2025-05-16 18:19:44 +0545

    changes in files

--- eslint.config.js
+++ eslint.config.js
@@ -1,53 +1,33 @@
-import babelParser from '@babel/eslint-parser';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import globals from 'globals';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
+  { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      parser: babelParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
         sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
-        requireConfigFile: false,
-        babelOptions: {
-          presets: ['@babel/preset-react'],
-        },
       },
     },
     plugins: {
-      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
     rules: {
-      'no-unused-vars': [
-        'warn',
-        { argsIgnorePattern: '^_', varsIgnorePattern: 'React|App' },
-      ],
-      quotes: ['warn', 'single', { avoidEscape: true }],
-      'react/prop-types': 'off',
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
-];
+]
